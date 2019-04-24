69f63b3e0ab173239407ea2bcedb9d73187a702d
Brianna <bkarp@seas.upenn.edu>
2019-04-23 22:51:21 -0400

    updated features

---
+++ extractFeatures_v2.m
@@ -0,0 +1,90 @@
+function features = extractFeatures_v1(ecog, fs)
+
+M = @(x) mean(x); % define mean function
+Area = @(x) sum(abs(x)); %area function (HW3) 
+Energy = @(x) sum(x.^2); %energy function (HW3) 
+LLfn = @(x) sum(abs(diff(x))); %line length (HW3)
+% ZX = @(x) sum(diff(sign(x - mean(x))) == 2 | diff(sign(x - mean(x))) == -2); %zero crossing (HW3) 
+numFeats = 9;     % number of features per channel
+
+winLen = 100e-3;    % window length (s)
+winDisp = 50e-3;    % window displacement (s)
+numWins = floor((size(ecog, 1)-winLen*fs) / (winDisp*fs)) + 1; % number of windows
+
+bands = [5 15; 20 25; 75 115; 125 160; 160 175];
+
+    function avgfreq = findFreq1(x)
+        L = winLen * fs;
+        f = fft(x);
+        P2 = abs(f/L);
+        P1 = P2(1:L/2+1);
+        P1(2:end-1) = 2*P1(2:end-1);
+        freq = fs*(0:(L/2))/L;
+        
+        ind = find((freq >= bands(1, 1)) & (freq <= bands(1, 2)));
+        avgfreq = mean(P1(ind));
+    end
+
+    function avgfreq = findFreq2(x)
+        L = winLen * fs;
+        f = fft(x);
+        P2 = abs(f/L);
+        P1 = P2(1:L/2+1);
+        P1(2:end-1) = 2*P1(2:end-1);
+        freq = fs*(0:(L/2))/L;
+        
+        ind = find((freq >= bands(2, 1)) & (freq <= bands(2, 2)));
+        avgfreq = mean(P1(ind));
+    end
+
+    function avgfreq = findFreq3(x)
+        L = winLen * fs;
+        f = fft(x);
+        P2 = abs(f/L);
+        P1 = P2(1:L/2+1);
+        P1(2:end-1) = 2*P1(2:end-1);
+        freq = fs*(0:(L/2))/L;
+        
+        ind = find((freq >= bands(3, 1)) & (freq <= bands(3, 2)));
+        avgfreq = mean(P1(ind));
+    end
+
+    function avgfreq = findFreq4(x)
+        L = winLen * fs;
+        f = fft(x);
+        P2 = abs(f/L);
+        P1 = P2(1:L/2+1);
+        P1(2:end-1) = 2*P1(2:end-1);
+        freq = fs*(0:(L/2))/L;
+        
+        ind = find((freq >= bands(4, 1)) & (freq <= bands(4, 2)));
+        avgfreq = mean(P1(ind));
+    end
+
+    function avgfreq = findFreq5(x)
+        L = winLen * fs;
+        f = fft(x);
+        P2 = abs(f/L);
+        P1 = P2(1:L/2+1);
+        P1(2:end-1) = 2*P1(2:end-1);
+        freq = fs*(0:(L/2))/L;
+        
+        ind = find((freq >= bands(5, 1)) & (freq <= bands(5, 2)));
+        avgfreq = mean(P1(ind));
+    end
+
+features = zeros(numWins, size(ecog, 2)*numFeats);
+
+for i = 1:size(ecog, 2) % iterate over number of channels
+    signal = ecog(:, i);
+    avFreq = MovingWinFeats(signal, fs, winLen, winDisp, M, Energy, Area, LLfn, ...
+        @findFreq1, @findFreq2, @findFreq3, @findFreq4, @findFreq5);
+    disp([num2str(i) ' ' num2str(length(avFreq))]) %for visualizing progress
+    avgFreq = reshape(avFreq, numFeats, numWins)';
+%     for j = 1:numFeats %normalizing 
+%         avgFreq(:, j) = (avgFreq(:, j) - mean(avgFreq(:, j))) / std(avgFreq(:,j));
+%     end 
+    column = (i-1)*numFeats+1;
+    features(:, column:(column+numFeats-1)) = avgFreq; 
+end
+end 
---
+++ linreg_v2.m
@@ -0,0 +1,52 @@
+function Y = linreg(features, labels, testing)
+% input:    features - matrix of predictors where rows = time bins, cols =
+% features for every channel for training
+%           labels - matrix of training glove positions for each finger
+%           testing - matrix to test model on
+% output: predictions of linear regression model
+
+N = 3;
+M = size(features, 1);
+numFeats = size(features, 2);
+featsPer = 9;                   % features/channel
+numChannels = numFeats/featsPer;       % assuming 6 features per channel
+
+% create R matrix for testing (if different than training)
+testM = size(testing, 1);
+testR = ones(testM-N, numFeats*N+1);
+if ~isequal(features, testing)
+    for i = 1:(testM-N+1)
+        for k = 1:numChannels       % over # channels
+            row = testing(i:i+N-1, ((k-1)*featsPer+1):((k-1)*featsPer+featsPer))';
+            row = reshape(row, 1, featsPer*N);
+            idx = (k-1)*N*featsPer+2;
+            testR(i, idx:idx+N*featsPer-1) = row;
+        end
+    end
+end
+
+R = ones(M-N, numFeats*N+1);
+%for each row
+for i = 1:(M-N+1)
+    % fixed pretty sure...
+    for j = 1:numChannels       % over # channels
+        row = features(i:i+N-1, ((j-1)*featsPer+1):((j-1)*featsPer+featsPer))';
+        row = reshape(row, 1, featsPer*N);
+        idx = (j-1)*N*featsPer+2;
+        R(i, idx:idx+N*featsPer-1) = row;
+    end
+end 
+
+a = R'*R;
+ainv = a \ eye(size(a, 1)); % need to compute inverse this way or else you get Inf 
+B = ainv*(R'*labels(N:end, :));
+
+% depending on testing data
+if isequal(features, testing)
+    Y = R*B;
+else
+    Y = testR*B;
+end
+
+end
+
