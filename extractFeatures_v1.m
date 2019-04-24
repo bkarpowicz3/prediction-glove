4c7fda8ace0bf6530605cc40f3c486fa4a678de9
Yueqi Ren <yueqir@gmail.com>
2019-04-24 15:10:56 -0400

    combined logistic and linear regressions but need fix

--- extractFeatures_v1.m
+++ extractFeatures_v1.m
@@ -10,63 +10,77 @@ numWins = floor((size(ecog, 1)-winLen*fs) / (winDisp*fs)) + 1; % number of windo
 bands = [5 15; 20 25; 75 115; 125 160; 160 175];
 
     function avgfreq = findFreq1(x)
-        L = winLen * fs;
-        f = fft(x);
-        P2 = abs(f/L);
-        P1 = P2(1:L/2+1);
-        P1(2:end-1) = 2*P1(2:end-1);
-        freq = fs*(0:(L/2))/L;
-        
-        ind = find((freq >= bands(1, 1)) & (freq <= bands(1, 2)));
-        avgfreq = mean(P1(ind));
+%         L = winLen * fs;
+%         f = fft(x);
+%         P2 = abs(f/L);
+%         P1 = P2(1:L/2+1);
+%         P1(2:end-1) = 2*P1(2:end-1);
+%         freq = fs*(0:(L/2))/L;
+%         
+%         ind = find((freq >= bands(1, 1)) & (freq <= bands(1, 2)));
+%         avgfreq = mean(P1(ind));
+        [s, f, t] = spectrogram(x, [], [], bands(1, 1):bands(1, 2), fs);
+        avgfreq = mean(mean(abs(s)));
     end
 
     function avgfreq = findFreq2(x)
-        L = winLen * fs;
-        f = fft(x);
-        P2 = abs(f/L);
-        P1 = P2(1:L/2+1);
-        P1(2:end-1) = 2*P1(2:end-1);
-        freq = fs*(0:(L/2))/L;
+        %         L = winLen * fs;
+        %         f = fft(x);
+        %         P2 = abs(f/L);
+        %         P1 = P2(1:L/2+1);
+        %         P1(2:end-1) = 2*P1(2:end-1);
+        %         freq = fs*(0:(L/2))/L;
+        %
+        %         ind = find((freq >= bands(2, 1)) & (freq <= bands(2, 2)));
+        %         avgfreq = mean(P1(ind));
         
-        ind = find((freq >= bands(2, 1)) & (freq <= bands(2, 2)));
-        avgfreq = mean(P1(ind));
+        [s, f, t] = spectrogram(x, [], [], bands(2, 1):bands(2, 2), fs);
+        avgfreq = mean(mean(abs(s)));
     end
 
     function avgfreq = findFreq3(x)
-        L = winLen * fs;
-        f = fft(x);
-        P2 = abs(f/L);
-        P1 = P2(1:L/2+1);
-        P1(2:end-1) = 2*P1(2:end-1);
-        freq = fs*(0:(L/2))/L;
-        
-        ind = find((freq >= bands(3, 1)) & (freq <= bands(3, 2)));
-        avgfreq = mean(P1(ind));
+%         L = winLen * fs;
+%         f = fft(x);
+%         P2 = abs(f/L);
+%         P1 = P2(1:L/2+1);
+%         P1(2:end-1) = 2*P1(2:end-1);
+%         freq = fs*(0:(L/2))/L;
+%         
+%         ind = find((freq >= bands(3, 1)) & (freq <= bands(3, 2)));
+%         avgfreq = mean(P1(ind));
+
+        [s, f, t] = spectrogram(x, [], [], bands(3, 1):bands(3, 2), fs);
+        avgfreq = mean(mean(abs(s)));
     end
 
     function avgfreq = findFreq4(x)
-        L = winLen * fs;
-        f = fft(x);
-        P2 = abs(f/L);
-        P1 = P2(1:L/2+1);
-        P1(2:end-1) = 2*P1(2:end-1);
-        freq = fs*(0:(L/2))/L;
-        
-        ind = find((freq >= bands(4, 1)) & (freq <= bands(4, 2)));
-        avgfreq = mean(P1(ind));
+%         L = winLen * fs;
+%         f = fft(x);
+%         P2 = abs(f/L);
+%         P1 = P2(1:L/2+1);
+%         P1(2:end-1) = 2*P1(2:end-1);
+%         freq = fs*(0:(L/2))/L;
+%         
+%         ind = find((freq >= bands(4, 1)) & (freq <= bands(4, 2)));
+%         avgfreq = mean(P1(ind));
+
+        [s, f, t] = spectrogram(x, [], [], bands(4, 1):bands(4, 2), fs);
+        avgfreq = mean(mean(abs(s)));
     end
 
     function avgfreq = findFreq5(x)
-        L = winLen * fs;
-        f = fft(x);
-        P2 = abs(f/L);
-        P1 = P2(1:L/2+1);
-        P1(2:end-1) = 2*P1(2:end-1);
-        freq = fs*(0:(L/2))/L;
-        
-        ind = find((freq >= bands(5, 1)) & (freq <= bands(5, 2)));
-        avgfreq = mean(P1(ind));
+%         L = winLen * fs;
+%         f = fft(x);
+%         P2 = abs(f/L);
+%         P1 = P2(1:L/2+1);
+%         P1(2:end-1) = 2*P1(2:end-1);
+%         freq = fs*(0:(L/2))/L;
+%         
+%         ind = find((freq >= bands(5, 1)) & (freq <= bands(5, 2)));
+%         avgfreq = mean(P1(ind));
+
+        [s, f, t] = spectrogram(x, [], [], bands(5, 1):bands(5, 2), fs);
+        avgfreq = mean(mean(abs(s)));
     end
 
 features = zeros(numWins, size(ecog, 2)*numFeats);
@@ -80,4 +94,5 @@ for i = 1:size(ecog, 2) % iterate over number of channels
     column = (i-1)*numFeats+1;
     features(:, column:(column+numFeats-1)) = avgFreq; 
 end
+
 end 
